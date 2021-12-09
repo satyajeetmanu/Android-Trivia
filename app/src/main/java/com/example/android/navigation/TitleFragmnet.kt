aa783988f7b530eacfd60034121247ce7b09afe1
Satyajeet Gaurav <satyajeetmanu@gmail.com>
2021-12-09 21:34:09 +0530

    added fragments to navigation graph

--- app/src/main/java/com/example/android/navigation/TitleFragmnet.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragmnet.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -37,6 +38,9 @@ class TitleFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_title,container,false)
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
         return binding.root
     }
 
