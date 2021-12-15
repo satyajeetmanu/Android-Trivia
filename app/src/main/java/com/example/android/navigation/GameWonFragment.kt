579b70952dc3a2f69d8dee50a4760c4f5eba906a
Satyajeet Gaurav <satyajeetmanu@gmail.com>
2021-12-15 11:22:56 +0530

    sent two arguments from game fragment to gameWon fragment using safe args

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -32,6 +33,8 @@ class GameWonFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
+        val args = GameWonFragmentArgs.fromBundle(arguments!!)
+        Toast.makeText(context,"Total: ${args.numQuestions}, Correct: ${args.numCorrect}",Toast.LENGTH_LONG).show()
         binding.nextMatchButton.setOnClickListener{
             it.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
         }
